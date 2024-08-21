705ca1f7cac2c02eb1649dfeff35d36f997afe16
IT24102375 <it24102375@my.sliit.lk>
2024-08-21 11:02:10 +0530

    IT24102375Lab4Q1

---
+++ Q1/IT24102375Lab4Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT24102375Lab4Q1{
+public static void main(String[] args){
+Scanner S =new Scanner(System.in);
+System.out.println("enter a number");
+int a= S.nextInt();
+if(a>0)
+System.out.println("the number is :positive");
+else if(a==0)
+System.out.println(" number is zero");
+else
+System.out.println("your number is negative");
+
+
+
+}
+}
